9366c6ed43abe21b345ad568f5a8cc36ac9bfc91
camael24 <thehawk@hoa-project.net>
2013-03-01 15:44:57 +0100

    Add sql file for SQLITE ... thks hywan :D

---
+++ Data/Variable/Database/database.sqlite.sql
@@ -0,0 +1,33 @@
+CREATE TABLE library (
+    idLibrary       INTEGER,
+    refUser         INTEGER,
+    name            VARCHAR(255),
+    description     VARCHAR(255),
+    home            VARCHAR(255),
+    release         VARCHAR(255),
+    documentation   VARCHAR(255),
+    time            TIMESTAMP,
+    valid           INTEGER,
+    PRIMARY KEY(idLibrary)
+);
+
+CREATE TABLE rang (
+    idRang          INTEGER,
+    RangLabel       VARCHAR(255),
+    RangClass       VARCHAR(255),
+    PRIMARY KEY(idRang)
+);
+
+CREATE TABLE user (
+    idUser          INTEGER,
+    username        VARCHAR(255),
+    password        VARCHAR(255),
+    email           VARCHAR(255),
+    rang            INTEGER,
+    PRIMARY KEY(idUser)
+
+);
+
+INSERT INTO rang( idRang, RangLabel, RangClass) VALUES (null , "Banned" ,"inverse");
+INSERT INTO rang( idRang, RangLabel, RangClass) VALUES (null , "Register","success");
+INSERT INTO rang( idRang, RangLabel, RangClass) VALUES (null , "Administrator","important");
