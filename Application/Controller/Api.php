e3f4af05159cf77cac976d369f33d06b4709d4ab
camael24 <thehawk@hoa-project.net>
2013-06-07 15:52:52 +0200

    Remove code artefact

--- Application/Controller/Api.php
+++ Application/Controller/Api.php
@@ -3,14 +3,8 @@
     }
     namespace Application\Controller {
 
-        use Hoathis\Context\Exception;
-
         class Api extends Generic
         {
-
-            private $_composerCache = 'hoa://Data/Variable/Cache/composer_';
-            private $_composerExt = '.json';
-
             public function AutocompleteActionAsync ()
             {
                 $library = new \Application\Model\Library();
@@ -30,8 +24,7 @@
 
             public function ComposerActionAsync ()
             {
-                $data  = $this->check('uri', true);
-                $repos = $this->check('gh-repos', null);
+                $data = $this->check('uri', true);
                 if ($data === null) {
                     echo json_encode(array('error' => 'wrong uri data'));
                 }
@@ -39,11 +32,6 @@
                     try {
 
                         $f = file_get_contents($data);
-                        if ($repos !== null and $repos !== '') {
-                            var_dump($repos);
-                            $file = $this->_composerCache . md5($repos) . $this->_composerExt;
-                            file_put_contents($file, $f);
-                        }
                         echo $f;
                     } catch (\Exception $e) {
                         echo json_encode(array('error' => 'The user / repo or the URI have a mistake please check it : ' . $data));
@@ -51,33 +39,6 @@
                 }
 
             }
-
-            public function updateAction ()
-            {
-                $query = $this->router->getQuery();
-                $user  = (array_key_exists('user', $query) ? $query['user'] : null);
-                $depot = (array_key_exists('depot', $query) ? $query['depot'] : null);
-
-                if ($user === null or $depot === null) {
-                    return;
-                }
-
-                $depot      = str_replace('_', '/', $depot);
-                $depot_Hash = md5($depot);
-                $depot_Uri  = $file = $this->_composerCache . $depot_Hash . $this->_composerExt;
-                $user_Model = new \Application\Model\User();
-
-                if ($user_Model->checkUser($user) === true or !file_exists($depot_Uri)) {
-                    return;
-                }
-
-                $composer = json_decode(file_get_contents($depot_Uri) , true);
-                echo '<pre>';
-                var_dump($composer);
-
-
-            }
-
 
         }
     }
